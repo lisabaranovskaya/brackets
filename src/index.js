26f5335c29ecc438c294435968626a47990ec63e
Lisa Baranovskaya <liza.baranovvvskaya@gmail.com>
2020-03-18 16:08:56 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length; ) {
+        if (!str.includes(bracketsConfig[i].join(''))) {
+            i++;
+        } else {
+            str = str.replace(bracketsConfig[i].join(''), '');
+            i = 0;
+        }
+    }
+
+    return (str.length === 0);
+
 }
